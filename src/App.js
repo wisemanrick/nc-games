5f9a26336d211320a8916715eb45cd6824e20625
Richard <wisemanrick@hotmail.co.uk>
2023-03-14 11:38:27 +0000

    4-List-all-reviews complete

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-
+import { Routes, Route } from "react-router-dom";
 import './App.css';
+import ShowAllReviews from "./components/allReviews";
+import Header from "./components/header";
+import UserLogin from "./components/login";
+import NavBar from "./components/nav";
+
 
 function App() {
   return (
     <div className="App">
-      <h1>Hi from APP!!</h1>
+      <Header />
+      <NavBar />
+
+      <Routes>
+        <Route path="/" element={<UserLogin />} />
+        <Route path="/reviews" element={<ShowAllReviews />} />
+
+
+      </Routes>
     </div>
   );
 }
---
+++ src/components/login.jsx
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom";
+
+function UserLogin () {
+    return (
+        <section className="login">
+        <h1>User login page</h1>
+        {/* <Link to="/reviews">Click to see reviews without loggin in</Link> */}
+        </section>
+
+    )
+}
+
+export default UserLogin
